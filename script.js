faf68847deb6b4f4b289bfff1c3722e3eac89fb7
KanatSeytov <55532407+KanatSeytov@users.noreply.github.com>
2019-12-24 00:08:13 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,30 @@
-var url = 'index.php';
 
-fetch(url,{
-	method: 'post',
-	body: {"name": "Cookies", "price":10000, "img":"images/cookie.jpg"}
-})
-.then(function (response) {
-  return response.text();
-})
-.then(function (body) {
-  console.log(body);
-});
+function request() {
+	// body...
+	var url = 'index.php';
+
+
+	var card;
+
+	fetch(url,
+	{
+	    headers: {
+
+	      'Accept': 'application/json',
+	      'Content-Type': 'application/json'
+	    },
+	    method: "POST",
+	    body: JSON.stringify({"action":"select","name": "Bread", "price":4000, "img":"images/bread.jpg"})
+	})
+	.then(function(response) {
+            if (response.status >= 200 && response.status < 300) {
+                console.log(response.text())
+            }
+            throw new Error(response.statusText)
+        })
+        .then(function(response) {
+            console.log(response);
+        })
+}
+
+
